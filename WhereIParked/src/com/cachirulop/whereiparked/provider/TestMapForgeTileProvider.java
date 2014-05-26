856c57526afc0c25f9d8078739ab89713c1e3ef2
cachirulop <dmagrom@gmail.com>
2014-05-26 22:58:58 +0200

    Reformat source code

--- WhereIParked/src/com/cachirulop/whereiparked/provider/TestMapForgeTileProvider.java
+++ WhereIParked/src/com/cachirulop/whereiparked/provider/TestMapForgeTileProvider.java
@@ -201,78 +201,35 @@ public class TestMapForgeTileProvider
                                       Math.PI) /
                                  2 * (1 << zoom));
     }
-/*
-    private byte[] readTileImage (int x,
-                                  int y,
-                                  int zoom)
-    {
-        // InputStream in = null;
-        FileInputStream in = null;
-        ByteArrayOutputStream buffer = null;
 
-        try {
-
-            in = new FileInputStream (getTileFile (x,
-                                                   y,
-                                                   zoom));
-            buffer = new ByteArrayOutputStream ();
-
-            int nRead;
-            byte[] data = new byte[BUFFER_SIZE];
-
-            while ((nRead = in.read (data,
-                                     0,
-                                     BUFFER_SIZE)) != -1) {
-                buffer.write (data,
-                              0,
-                              nRead);
-            }
-            buffer.flush ();
-
-            return buffer.toByteArray ();
-        }
-        catch (IOException e) {
-            e.printStackTrace ();
-            return null;
-        }
-        catch (OutOfMemoryError e) {
-            e.printStackTrace ();
-            return null;
-        }
-        finally {
-            if (in != null)
-                try {
-                    in.close ();
-                }
-                catch (Exception ignored) {
-                }
-            if (buffer != null)
-                try {
-                    buffer.close ();
-                }
-                catch (Exception ignored) {
-                }
-        }
-    }
-
-    private int fixYCoordinate (int y,
-                                int zoom)
-    {
-        int size = 1 << zoom; // size = 2^zoom
-        return size - 1 - y;
-    }
-
-    private File getTileFile (int x,
-                              int y,
-                              int zoom)
-    {
-        File sdcard = Environment.getExternalStorageDirectory ();
-        String tileFile = "/maps/parking.png";
-        File file = new File (sdcard,
-                              tileFile);
-        return file;
-    }
-*/
+    /*
+     * private byte[] readTileImage (int x, int y, int zoom) { // InputStream in
+     * = null; FileInputStream in = null; ByteArrayOutputStream buffer = null;
+     * 
+     * try {
+     * 
+     * in = new FileInputStream (getTileFile (x, y, zoom)); buffer = new
+     * ByteArrayOutputStream ();
+     * 
+     * int nRead; byte[] data = new byte[BUFFER_SIZE];
+     * 
+     * while ((nRead = in.read (data, 0, BUFFER_SIZE)) != -1) { buffer.write
+     * (data, 0, nRead); } buffer.flush ();
+     * 
+     * return buffer.toByteArray (); } catch (IOException e) { e.printStackTrace
+     * (); return null; } catch (OutOfMemoryError e) { e.printStackTrace ();
+     * return null; } finally { if (in != null) try { in.close (); } catch
+     * (Exception ignored) { } if (buffer != null) try { buffer.close (); }
+     * catch (Exception ignored) { } } }
+     * 
+     * private int fixYCoordinate (int y, int zoom) { int size = 1 << zoom; //
+     * size = 2^zoom return size - 1 - y; }
+     * 
+     * private File getTileFile (int x, int y, int zoom) { File sdcard =
+     * Environment.getExternalStorageDirectory (); String tileFile =
+     * "/maps/parking.png"; File file = new File (sdcard, tileFile); return
+     * file; }
+     */
     /**
      * For some reason, if we just use InternalRenderTheme.OSMARENDER, it fails
      * to work (the XML parser gets a NULL as the input stream). It must be
